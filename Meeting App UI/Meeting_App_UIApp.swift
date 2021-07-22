18fd72923e9bdce73b71c908f85a3abd7f41a5c7
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-22 22:36:05 +0200

    Initial Commit

---
+++ Meeting App UI/Meeting_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_App_UIApp.swift
+//  Meeting App UI
+//
+//  Created by Michele Manniello on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Meeting_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
